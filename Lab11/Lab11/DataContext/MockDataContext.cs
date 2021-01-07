59191b6952ed795d505941af9d8a9a001aeac625
AltiTorie <Arek.ste99@wp.pl>
2021-01-07 09:18:53 +0100

    Views updated

--- Lab11/Lab11/DataContext/MockDataContext.cs
+++ Lab11/Lab11/DataContext/MockDataContext.cs
@@ -8,40 +8,45 @@ namespace Lab11.DataContext
 {
     public class MockDataContext : IDataContext
     {
-
-        List<UserViewModel> users = new List<UserViewModel>()
+        Dictionary<int, UserViewModel> users = new Dictionary<int, UserViewModel>()
         {
-            new UserViewModel(0,"Arek","Arek@gmail.com", "12-345", Category.Admin),
-            new UserViewModel(1, "Tomek", "Tom123@yahoo.com", "96-243", Category.Buyer)
+            { 0, new UserViewModel(0,"Arek321","Arek@gmail.com", "12-345", Category.Admin) },
+            { 1,  new UserViewModel(1, "Tomek", "Tom123@yahoo.com", "96-243", Category.Buyer) },
+            { 2,  new UserViewModel(2, "Potomek", "PamelaAnderson@gmail.com", "75-143", Category.Moderator) },
+            { 3,  new UserViewModel(3, "Anijapania", "JakisEmail@emailowo.com", "671-24", Category.Seller) }
         };
         public void AddUser(UserViewModel person)
         {
-            int nextId = users.Max(u => u.Id) + 1;
+            int nextId = users.Max(u => u.Key) + 1;
             person.Id = nextId;
-            users.Add(person);
+            users.Add(nextId, person);
         }
 
         public UserViewModel GetUser(int id)
         {
-            return users.FirstOrDefault(u => u.Id == id);
+            return users.FirstOrDefault(u => u.Key == id).Value;
         }
 
         public List<UserViewModel> GetUsers()
         {
-            return users;
+            return users
+                .Select(u => u.Value)
+                .ToList<UserViewModel>();
         }
 
         public void RemoveUser(int id)
         {
-            UserViewModel userToRemove = users.FirstOrDefault(u => u.Id == id);
-            if (userToRemove != null)
-                users.Remove(userToRemove);
+            if (users.ContainsKey(id))
+                users.Remove(id);
         }
 
         public void UpdateUser(UserViewModel person)
         {
-            UserViewModel userToUpdate = users.FirstOrDefault(u => u.Id == person.Id);
-            users = users.Select(u => (u.Id == person.Id) ? person : u).ToList();
+            if (users.ContainsKey(person.Id))
+            {
+                users[person.Id] = person;
+            }
+            else throw new Exception("User does not exist");
         }
     }
 }
